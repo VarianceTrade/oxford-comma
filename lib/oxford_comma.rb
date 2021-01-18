2e018e57b0c15703b88d83ac94e92bba09d6b1b0
Brandon <sugb73@gmail.com>
2021-01-18 17:23:42 -0500

    commit tree 09306b4bbc0c4368dfaeb398e3206b9f8a3bfe6f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
-
+    # ["a", "b", "c"].join => "abc"
+    # ["a", "b", "c"].join(" ") => "a b c"
+    # ["a", "b", "c"].join(", ") => "a, b, c"
+    if array.length == 1 # ["a"]
+        return array[0] # => "a"
+    elsif array.length == 2  # ["a", "b"] => "a and b"
+        return array.join(" and ")
+    elsif array.length > 2
+        # ["a", "b", "c"] => "a, b, and c"
+        # ["a", "b", "c", "d"] => "a, b, c, and d"
+        # ["a", "b", "c", "d", "e"] => "a, b, c, d, and e"
+        last_element = array.pop # => "d"
+        # array => ["a", "b", "c"]
+        first_half = array.join(", ") # => "a, b, c"
+        second_half = ", and #{last_element}" # => ", and d"
+        return first_half + second_half
+    end
 end
+
+# ? .each puts ", " ?
+# ? .each puts "and " ?
+
+
+
+# ? _index ?
